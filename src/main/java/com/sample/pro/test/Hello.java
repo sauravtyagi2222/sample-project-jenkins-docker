4be0575c385200f41f0403d1a06cc09b4a6de4eb
Saurav Tyagi <Sauravtyagi2222@gmail.com>
2020-07-12 06:30:43 -0700

    CICD build

--- src/main/java/com/sample/pro/test/Hello.java
+++ src/main/java/com/sample/pro/test/Hello.java
@@ -3,6 +3,6 @@ package com.sample.pro.test;
 public class Hello {
 public static void main(String[] args) {
 	System.out.println("Hello test");
-	System.out.println("Hello");
+	System.out.println("Hello test1");
 }
 }
